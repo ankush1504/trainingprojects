a6d1b59a1ed5a5fa1d5dcfe7f285a76f8851243f
ankush1504 <55706795+ankush1504@users.noreply.github.com>
2019-10-17 16:26:29 +0530

    assignment on calculating the area of shapes and product details assignment

---
+++ constructorassignment/constructorassignment/Product.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructorassignment
+{
+    class Product
+    {
+        static void Main(string[] args)
+        {
+            string[] products = new string[] { "pen", "pencil", "eraser", "scale" };
+            int[] price = new int[] { 10, 5, 3, 15 };
+            Console.WriteLine("Product Details:");
+            Console.WriteLine("product \t price \t");
+            Console.WriteLine();
+            for (int i = 0; i < 4; i++)
+            {
+
+                Console.Write("{0} \t\t {1}\n", products[i], price[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("enter number of items to buy:");
+            int itemquant = int.Parse(Console.ReadLine());
+            string[] selectitem = new string[itemquant];
+            Console.WriteLine("enter the products to buy");
+            for (int k = 0; k < itemquant; k++)
+            {
+                selectitem[k] = Console.ReadLine();
+            }
+
+
+            int quantity, count = 0 ;
+            int total = 0;
+            string flag = "false";
+            foreach (var item in selectitem)
+            { 
+                for (int j = 0; j < 4; j++)
+                {
+                    if (item.ToLower() == products[j])
+                    {
+                        Console.WriteLine("enter {0} quantity:",products[j]);
+                        quantity = int.Parse(Console.ReadLine());
+                        total = total + quantity * price[j];
+                        flag = "true";
+                        break;
+                    }
+                    count++;
+                }
+                if(count==4)
+                {
+                    Console.WriteLine("the product {0} is not available",item);
+                }
+            }
+           
+            if (flag=="false")
+            {
+                Console.WriteLine("product not found");
+            }
+            else
+            {
+                Console.WriteLine("total price is:{0}", total);
+            }
+            Console.ReadLine();
+   
+
+        }
+    }
+}
---
+++ constructorassignment/constructorassignment/Shapes.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructorassignment
+{
+    
+    class Shapes
+    {
+        int side,len,bre;
+        double rad, a, b, c;
+        Shapes(int side)
+        {
+            this.side = side;
+            Console.WriteLine("Area of square is:{0}",this.side*this.side);
+        }
+        Shapes(int len,int bre)
+        {
+            this.len = len;
+            this.bre = bre;
+            Console.WriteLine("Area of Rectangle is:{0}", this.len * this.bre);
+        }
+        Shapes(double rad)
+        {
+            this.rad = rad;
+            Console.WriteLine("Area of circle is:{0}",3.14*this.rad*this.rad);
+        }
+        Shapes(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            double s,ar;
+            s = (this.a +this.b +this.c) / 2;
+            ar = s * (s - this.a) * (s - this.b) * (s - this.c);
+            ar = Math.Pow(ar, 0.5);
+            Console.WriteLine("Area of rectangle is:{0}",ar);
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter the following options to calculate area of respective shapes\n");
+            Console.WriteLine("1.Square\t 2.Rectangle\t 3.Circle\t 4.Triangle");
+            int choice = int.Parse(Console.ReadLine());
+            switch(choice)
+            {
+                case 1: Console.WriteLine("enter side of a square");
+                    int side = int.Parse(Console.ReadLine());
+                    Shapes a1 = new Shapes(side);
+                    break;
+                case 2:
+                    Console.WriteLine("enter length and breadth of Rectangle");
+                    int len = int.Parse(Console.ReadLine());
+                    int bre = int.Parse(Console.ReadLine());
+                    Shapes a2 = new Shapes(len,bre);
+                    break;
+                case 3:
+                    Console.WriteLine("enter Radius of a Circle");
+                    double rad = Convert.ToDouble(Console.ReadLine());
+                    Shapes a3 = new Shapes(rad);
+                    break;
+                case 4:
+                    Console.WriteLine("enter sides of Rectangle");
+                    int a = int.Parse(Console.ReadLine());
+                    int b = int.Parse(Console.ReadLine());
+                    int c = int.Parse(Console.ReadLine());
+                    Shapes a4 = new Shapes(a, b,c);
+                    break;
+                default:
+                    Console.WriteLine("invalid entry");
+                    break;
+
+            }
+            Console.ReadLine();
+                
+        }
+    }
+}
