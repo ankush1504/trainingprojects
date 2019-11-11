ae5552afc324add2008e74acd2fc38012c234c87
ankush1504 <55706795+ankush1504@users.noreply.github.com>
2019-11-11 22:20:15 +0530

    Assessment on displaying employees in a particular department using ASP .net web application

---
+++ EntityFrameworkAssessment/EntityFrameworkAssessment/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EntityFrameworkAssessment.Models;
+
+
+namespace EntityFrameworkAssessment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(DepartmentEmployee demp)
+        {
+            DBfirstDemoEntities con = new DBfirstDemoEntities();
+            ViewBag.Departments = new SelectList(con.Departments, "Id", "Name");
+            demp.dep1 = con.Departments.ToList();
+            demp.emp1 = con.Employees.ToList();
+           
+            return View(demp);
+        }
+        [HttpPost]
+        public ActionResult Index(DepartmentEmployee demp, FormCollection form)
+        {
+
+            DBfirstDemoEntities con = new DBfirstDemoEntities();
+            ViewBag.Departments = new SelectList(con.Departments, "Id", "Name");
+            var mymodel = new DepartmentEmployee();
+            mymodel.dep1 = con.Departments.ToList();
+            mymodel.emp1 = con.Employees.ToList();
+            
+
+            mymodel.ind1= int.Parse(form["selectedDepartment"]);
+
+            //var deptlist = con.Departments;
+            //List<DepartmentEmployee> empnames1 = new List<DepartmentEmployee>();
+            //var emplist = con.Employees;
+
+
+            return View(mymodel);
+        }
+    
+     
+
+
+
+    }
+}
---
+++ EntityFrameworkAssessment/EntityFrameworkAssessment/Models/DepartmentEmployee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkAssessment.Models
+{
+    public class DepartmentEmployee
+    {
+        public int ind1 { set; get; }
+      public IEnumerable<Employee> emp1 { get; set; }
+
+        public IEnumerable<Department> dep1 { get; set; }
+
+
+    }
+}
